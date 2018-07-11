0777e8a7fa1dd95995cc472e60639ea1d899636b
Vivek Choksi <mail.vivekchoksi@gmail.com>
2018-07-11 14:25:12 +0530

    WIP

--- lib/soa_wrapper.rb
+++ lib/soa_wrapper.rb
@@ -5,10 +5,7 @@ require 'excon'
 
 module SoaWrapper
   def self.soa_wrapper
-    puts "Hello World"
     config = OpenStruct.new(YAML.load_file(Rails.root.join("config", "soa_wrapper.yml")))
-    url = config.Eko['path']
-    response = Excon.get(url)
-    puts response.body
+    @response = Excon.get(config.Eko['path'])
   end
 end
